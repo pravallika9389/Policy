94bf2c029a5c814c5b19efe5b8d44f59799da53a
unknown <pravallika.ragipani@gmail.com>
2018-08-01 23:54:00 +1000

    first commit

--- configuration.js
+++ configuration.js
@@ -3,9 +3,9 @@ var configuration = {
     port: 9064,
   },
   mongo: {
-    development: {connectionString: 'mongodb://localhost:27017/policy',},
-    test: {connectionString: 'mongodb://localhost:27017/policy-test',},
-    production: {connectionString: 'mongodb://pravallika.ragipani@gmail.com:pp65794276@ds111336.mlab.com:11336/policy',},
+    development: {connectionString: 'mongodb://localhost:27017/codetest',},
+    test: {connectionString: 'mongodb://localhost:27017/code-test',},
+    production: {connectionString: 'mongodb://pravallika.ragipani@gmail.com:pp65794276@ds111336.mlab.com:11336/codetest',},
   },
 };
 
